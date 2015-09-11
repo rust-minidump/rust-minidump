2472b35c83d403373c3fcee23fdc89f689828b9b
Ted Mielczarek <ted@mielczarek.org>
2015-09-10 20:23:06 -0400

    silence warnings in bindgen'ed code

--- bindgen.sh
+++ bindgen.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 dir=`dirname $0`
-$dir/../rust-bindgen/target/debug/bindgen -I $dir/../google-breakpad/src/ $dir/../google-breakpad/src/google_breakpad/common/minidump_format.h > src/minidump_format.rs
+(echo "#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
+"; $dir/../rust-bindgen/target/debug/bindgen -I $dir/../breakpad/src/src/ $dir/../breakpad/src/src/google_breakpad/common/minidump_format.h) > src/minidump_format.rs
