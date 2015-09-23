da085f6bcd6278b594c2911a51c1ca6bc012c22c
Ted Mielczarek <ted@mielczarek.org>
2015-09-23 12:49:11 -0400

    Add a stub minidump_dump

---
+++ src/bin/minidump_dump.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+
+extern crate minidump_processor;
+
+use minidump_processor::*;
+
+fn print_minidump_dump(path : &str) {
+    let f = File::open(&path).ok().expect(&format!("failed to open file: {:?}", path));
+    match Minidump::read(f) {
+        Ok(dump) => {
+            println!("OK");
+        },
+        Err(err) => {
+            let mut stderr = std::io::stderr();
+            writeln!(&mut stderr, "Error reading dump").unwrap();
+        },
+    }
+}
+
+fn main() {
+    if let Some(path) = env::args().nth(1) {
+        print_minidump_dump(&path);
+    } else {
+        let mut stderr = std::io::stderr();
+        writeln!(&mut stderr, "Usage: minidump_dump <minidump>").unwrap();
+    }
+}
