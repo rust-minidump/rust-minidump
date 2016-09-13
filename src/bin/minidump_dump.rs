7cfbbc591a97f7833ef38044d77b996761ef9a9d
Ted Mielczarek <ted@mielczarek.org>
2016-09-13 06:04:57 -0400

    Dump Linux streams in minidump_dump

--- src/bin/minidump_dump.rs
+++ src/bin/minidump_dump.rs
@@ -3,13 +3,29 @@
 
 use std::env;
 use std::path::Path;
-use std::io::Write;
+use std::io::{self,Write};
+use std::str;
 
 extern crate minidump;
 
 use minidump::*;
 
 const USAGE : &'static str = "Usage: minidump_dump <minidump>";
+
+macro_rules! streams {
+    ( $( $x:ident ),* ) => {
+        &[$( ( minidump_format::$x, stringify!($x) ) ),*]
+    };
+}
+
+fn print_raw_stream<T: Write>(name: &str, contents: Vec<u8>, out: &mut T) -> io::Result<()> {
+    try!(writeln!(out, "Stream {}:", name));
+    let s = &contents.split(|&v| v == 0)
+        .map(|s| String::from_utf8_lossy(s))
+        .collect::<Vec<_>>()
+        .join("\\0\n");
+    write!(out, "{}\n\n", s)
+}
 
 fn print_minidump_dump(path : &Path) {
     match Minidump::read_path(path) {
@@ -37,7 +53,18 @@ fn print_minidump_dump(path : &Path) {
                 breakpad_info.print(stdout).unwrap();
             }
             // TODO: MemoryInfoList
-            // TODO: raw Linux streams
+            for &(stream, name) in streams!(
+                MD_LINUX_CMD_LINE,
+                MD_LINUX_ENVIRON,
+                MD_LINUX_LSB_RELEASE,
+                MD_LINUX_PROC_STATUS,
+                MD_LINUX_CPU_INFO,
+                MD_LINUX_MAPS
+                    ) {
+                if let Ok(contents) = dump.get_raw_stream(stream) {
+                    print_raw_stream(name, contents, stdout).unwrap();
+                }
+            }
         },
         Err(err) => {
             let mut stderr = std::io::stderr();
